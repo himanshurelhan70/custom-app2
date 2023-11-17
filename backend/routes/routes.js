c5b785b74ebc8919b6d38d4c5ac6f52d698db230
himanshurelhan70 <himanshurelh70@gmail.com>
2023-11-17 11:55:26 +0530

    garvit code

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const FormData = require('form-data');
+
+// 
+const axios = require('axios');
 
 // authentication and authorization controllers
 const {login, signup, logout} = require('../controllers/Auth');
@@ -42,5 +46,64 @@ router.get('/', (req, res) => {
 })
 
 
+// garvit 
+var zapikey = "1003.eec8614a7ccdc1ed88053beb3bae5b5e.e507e1d4b470523a54606c5e9809b86a";
+
+
+router.post('/update1',async(req,res)=>{
+    try {
+        const data = req.body;
+        console.log("data", data)
+    
+        function jsonToFormData(json) {
+          const from = new FormData();
+    
+          for (const key in json) {
+            
+            if (json.hasOwnProperty(key)) {
+                // console.log("object", key, json[key]);
+              from.append(key, json[key]);
+            }
+          }
+    
+          return from;
+        }
+        const finalPostData = jsonToFormData(data);
+        const URL =
+        "https://www.zohoapis.com/crm/v2/functions/addDataToCarLoan1/actions/execute?auth_type=apikey&zapikey="+zapikey;
+        const response = await axios.post(URL, finalPostData);
+        console.log(response.data)
+        res.json({ data: response.data });
+      } catch (error) {
+        res.json({ error: error.message });
+      }
+});
+
+
+router.post('/update2',async(req,res)=>{
+  try {
+      const data = req.body;
+  
+      function jsonToFormData(json) {
+        const from = new FormData();
+  
+        for (const key in json) {
+          if (json.hasOwnProperty(key)) {
+            from.append(key, json[key]);
+          }
+        }
+  
+        return from;
+      }
+      const finalPostData = jsonToFormData(data);
+      const URL =
+      "https://www.zohoapis.com/crm/v2/functions/addDataToCarLoan2/actions/execute?auth_type=apikey&zapikey="+zapikey;
+      const response = await axios.post(URL, finalPostData);
+      console.log(response.data)
+      res.json({ data: response.data });
+    } catch (error) {
+      res.json({ error: error.message });
+    }
+})
 
 module.exports = router;
